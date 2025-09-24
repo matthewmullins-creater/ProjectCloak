f9d3b22d782a4e6c86fc22ae231fba13ca4f2ddb
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-24 17:21:02 -0500

    CorsConfig and ComandLineStartupRunner update

---
+++ backend/src/main/java/site/projectcloak/backend/CommandLineStartupRunner.java
@@ -0,0 +1,73 @@
+package site.projectcloak.backend;
+
+import site.projectcloak.backend.Role.Role;
+import site.projectcloak.backend.Role.RoleRepository;
+import site.projectcloak.backend.User.User;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import site.projectcloak.backend.User.UserService;
+
+@Component
+public class CommandLineStartupRunner implements CommandLineRunner {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Value("${PGADMIN_DEFAULT_PASSWORD}")
+    private String password;
+
+    @Value("${PGADMIN_DEFAULT_EMAIL}")
+    private String email;
+
+    /** 
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        // create roles
+
+        Role user = new Role();
+        user.setName("USER");
+        if (roleRepository.findByName(user.getName()).isEmpty()) {
+            roleRepository.save(user);
+        }
+
+        Role blogger = new Role();
+        blogger.setName("BLOGGER");
+        if (roleRepository.findByName(blogger.getName()).isEmpty()) {
+            roleRepository.save(blogger);
+        }
+
+        Role admin = new Role();
+        admin.setName("ADMIN");
+        if (roleRepository.findByName(admin.getName()).isEmpty()) {
+            admin = roleRepository.saveAndFlush(admin);
+        }
+
+        roleRepository.flush();
+
+        String username = "stealthninja";
+        if (userService.findUserByUsername(username).isEmpty()) {
+
+            User admin_user = new User(username, null, null, password);
+            admin_user.setEmail(email);
+
+            Role adminRole = roleRepository.findByName("ADMIN").get();
+            admin_user.setRoles(Collections.singleton(adminRole));
+
+            userService.saveUser(admin_user);
+        }
+
+    }
+
+}
