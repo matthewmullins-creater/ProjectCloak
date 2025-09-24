8a08ced614ed8675889c5af7b0480e176eedb940
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-24 17:58:44 -0500

    Create Secruity JWT controller

---
+++ backend/src/main/java/site/projectcloak/backend/Security/WebSecurityConfig.java
@@ -0,0 +1,81 @@
+package site.projectcloak.backend.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import site.projectcloak.backend.Security.JWT.JwtAuthFilter;
+import site.projectcloak.backend.User.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    /** 
+     * @return PasswordEncoder
+     */
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /** 
+     * @return AuthenticationManager
+     */
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        ProviderManager providerManager = new ProviderManager(authenticationProvider);
+        providerManager.setEraseCredentialsAfterAuthentication(false);
+
+        return providerManager;
+
+    }
+
+    /** 
+     * @param httpSecurity
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new AngularCsrfTokenRequestHandler()))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll()
+                        .requestMatchers("/blog/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/profile/**").hasAnyRole("USER", "ADMIN", "BLOGGER")
+                        .requestMatchers("/blog/create/**").hasAnyRole("BLOGGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/blog/create")
+                        .hasAnyRole("BLOGGER", "ADMIN").anyRequest().authenticated()
+
+                ).httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+}
