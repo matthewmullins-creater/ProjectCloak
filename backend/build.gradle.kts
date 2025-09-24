2fccaeaca0450fd82a232ce64d94700e7b76a606
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-24 18:01:37 -0500

    backend gradle config files

---
+++ backend/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "site.projectcloak"
+description = "Backend for projectcloak.site"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+	implementation("io.github.cdimascio:dotenv-java:3.0.0")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.jar {
+	archiveBaseName.set("backend")
+}
