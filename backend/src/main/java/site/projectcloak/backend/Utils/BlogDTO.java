66479beccc1e2fb7f63e9cf86a2fd7d7c9fc9416
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-24 18:00:53 -0500

    Update backend utils  files

---
+++ backend/src/main/java/site/projectcloak/backend/Utils/BlogDTO.java
@@ -0,0 +1,46 @@
+package site.projectcloak.backend.Utils;
+
+import java.util.Set;
+
+public class BlogDTO {
+    private String title;
+    private Set<String> tags;
+    private String content;
+
+    /** 
+     * @return String
+     */
+    public String getTitle() {
+        return title;
+    }
+    /** 
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    /** 
+     * @return Set<String>
+     */
+    public Set<String> getTags() {
+        return tags;
+    }
+    /** 
+     * @param tags
+     */
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+    /** 
+     * @return String
+     */
+    public String getContent() {
+        return content;
+    }
+    /** 
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ backend/src/main/java/site/projectcloak/backend/Utils/LoginDTO.java
@@ -0,0 +1,41 @@
+package site.projectcloak.backend.Utils;
+
+
+public class LoginDTO {
+    private String username;
+
+    private String password;
+
+
+    /** 
+     * @return String
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+
+    /** 
+     * @return String
+     */
+    public String getPassword() {
+        return this.password;
+    }
+
+    /** 
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    /** 
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
