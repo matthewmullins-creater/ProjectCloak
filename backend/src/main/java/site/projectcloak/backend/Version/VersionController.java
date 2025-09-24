26e7d536c870c48d733dde75a74cc971e6b39fa3
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-24 18:01:12 -0500

    baclend version matching logic

---
+++ backend/src/main/java/site/projectcloak/backend/Version/VersionController.java
@@ -0,0 +1,49 @@
+package site.projectcloak.backend.Version;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.SpringVersion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@RestController
+@RequestMapping("/api/v1/version")
+public class VersionController {
+    @Autowired
+    ObjectMapper mapper;
+
+    
+    /** 
+     * @return ObjectNode
+     */
+    @GetMapping("spring")
+    ObjectNode spring() {
+        ObjectNode versionObject = mapper.createObjectNode();
+        versionObject.put("name", "spring");
+        versionObject.put("version", SpringVersion.getVersion());
+        return versionObject;
+    }
+
+    /** 
+     * @return ObjectNode
+     */
+    @GetMapping("java")
+    ObjectNode java() {
+        ObjectNode versionObject = mapper.createObjectNode();
+        versionObject.put("name", "java");
+        versionObject.put("version", Runtime.version().toString());
+        return versionObject;
+    }
+
+    /** 
+     * @return String
+     */
+    @GetMapping("postgresql")
+    String postgresql() {
+        return "";
+    }
+}
