0bae6f1bbff1b3551aa484af232734e1dfc8b5a4
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-25 00:33:49 -0500

    Update Application.java

--- backend/src/main/java/site/projectcloak/backend/Application.java
+++ backend/src/main/java/site/projectcloak/backend/Application.java
@@ -12,9 +12,6 @@ public class Application {
      * @param args
      */
 	public static void main(String[] args) {
-		// Load .env file so Spring can use its variables
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(Application.class, args);
 	}
 
